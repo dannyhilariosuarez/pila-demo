d2304395fd2d39834e2e6a6f8b2f009f19678ded
Danny Hilario Suarez <dannyhilariosuarez@gmail.com>
2026-03-11 10:22:26 -0400

    feat: add rate limiting middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { generateId, formatTimestamp, sanitizeString } from './utils';
+import { rateLimiter } from './middleware/rateLimiter';
 
 const app = express();
 app.use(express.json());
+app.use(rateLimiter);
 
 interface Item {
   id: string;
